e6f2a371bf903ac4e6335ebd37f10fd3411f308a
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-02-06 22:12:19 -0500

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+//webpack.config.js
+
+const path = require('path');
+const webpack = require('html-webpack-plugin');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack({
+            template: './index.html',
+        }),
+    ],
+};
