d7592be7b6bf7a09ccc15babccad221058fe80e0
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-02-06 22:53:09 -0500

    Adjustments to the file paths

--- react/roots/components/PageContainer/index.js
+++ react/roots/components/PageContainer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from 'react/roots/components/Footer'
+import Footer from '../Footer/index.js'
 
 const PageContainer = ({ children, title }) => {
   return (
