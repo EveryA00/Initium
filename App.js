b3e277159106e8dd5bda2bad2f2d2faf7faaf177
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-02-06 22:24:31 -0500

    Second Commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // App.js
 
 import React, { Component } from 'react';
+import PageContainer from './react/roots/components/PageOne';
+
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
+            <PageContainer title="First Project"/>
         );
     }
 }
