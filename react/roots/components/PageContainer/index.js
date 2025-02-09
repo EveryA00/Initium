edba398d1ac16002b9aa208e6ce1cd389c6b6a7d
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-02-08 21:52:32 -0500

    Added styles

--- react/roots/components/PageContainer/index.js
+++ react/roots/components/PageContainer/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Styled } from './styledComponents.js'
 import Footer from '../Footer/index.js'
 
 const PageContainer = ({ children, title }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
+    <Styled.PageContainer className="min-h-screen flex flex-col bg-gray-100">
       <header className="bg-blue-600 text-white p-4 text-xl font-bold">
         {title || "My App"}
       </header>
       <main className="flex-grow p-6">{children}</main>
      <Footer></Footer>
-    </div>
+    </Styled.PageContainer>
   );
 };
 
---
+++ react/roots/components/PageContainer/styledComponents.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// Styled component named StyledButton
+const PageContainer = styled.div`
+  background-color: #fff;
+`;
+
+export const Styled = {
+    PageContainer,
+  };
