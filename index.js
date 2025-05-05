cc077394be57213a98f139550cf7e3654ccded63
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-04 22:09:15 -0400

    another adjustment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import from react-dom/client
-import App from './MyApp/src/App.js';
+import App from './src/App.js';
 import { ThemeProvider } from 'styled-components';
-import { AppProvider } from './MyApp/src/context/context.js';  // Import the Context Provider
-import theme from './MyApp/src/theme.js';
+import { AppProvider } from './src/context/context.js';  // Import the Context Provider
+import theme from './src/theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
 
