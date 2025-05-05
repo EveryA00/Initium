04c7aa4a46b025a091321a4e4c14d8bb98748b75
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-04 21:57:06 -0400

    update stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navigation from './src/react/roots/components/Navigation/index.js';
-import Footer from './src/react/roots/components/Footer/index.js';
-import About from './src/react/roots/pages/About/index.js';
-import Bag from './src/react/roots/pages/Bag/index.js';
-import Contact from './src/react/roots/pages/Contact/index.js';
-import Home from './src/react/roots/pages/Home/index.js';
-import SignIn from './src/react/roots/pages/SignIn/index.js';
-import ProductGrid from './src/react/roots/pages/ProductGrid/index.js';
-import ProductDetail from './src/react/roots/pages/ProductDetailsPage/index.js';
-import GlobalStyle from './src/styles/globalStyles.js';
-import { ProductsProvider } from './src/context/ProductsContext'
+import Navigation from './react/roots/components/Navigation/index.js';
+import Footer from './react/roots/components/Footer/index.js';
+import About from './react/roots/pages/About/index.js';
+import Bag from './react/roots/pages/Bag/index.js';
+import Contact from './react/roots/pages/Contact/index.js';
+import Home from './react/roots/pages/Home/index.js';
+import SignIn from './react/roots/pages/SignIn/index.js';
+import ProductGrid from './react/roots/pages/ProductGrid/index.js';
+import ProductDetail from './react/roots/pages/ProductDetailsPage/index.js';
+import GlobalStyle from './styles/globalStyles.js';
+import { ProductsProvider } from './context/ProductsContext.js'
 
 const App = () => {
       const [cart, setCart] = useState([]);
