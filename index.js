057f9c648c9e0365a525226aa09031a119ffc38c
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-04 21:48:24 -0400

    Update src file.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import from react-dom/client
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { AppProvider } from './src/context/context.js';  // Import the Context Provider
-import theme from './src/theme';
+import { AppProvider } from '@/context/context.js';  // Import the Context Provider
+import theme from '@/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
 
