d97fc7affd862baf90e6ae5c28cdd59f22523fbe
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-08 22:43:49 -0400

    update the eslint

--- next-backend/eslint.config.mjs
+++ next-backend/eslint.config.mjs
@@ -5,7 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const eslintConfig = {
-  extends: ["next/core-web-vitals"],
+  extends: ["next/core-web-vitals"],  // Extend Next.js' core-web-vitals config
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
 };
 
 export default eslintConfig;
