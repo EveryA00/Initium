4160ec4c88ff468a09604cc41826b686fa62f735
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-08 22:49:03 -0400

    update esling

--- next-backend/eslint.config.mjs
+++ next-backend/eslint.config.mjs
@@ -1,14 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = {
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      extends: ['next/core-web-vitals'],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+  ],
+};
 
 export default eslintConfig;
