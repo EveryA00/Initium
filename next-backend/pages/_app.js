9a85c1f0489537ef8e7a9a82a18a7542ff396ba6
EveryA00 <42818845+EveryA00@users.noreply.github.com>
2025-05-11 21:27:18 -0400

    Add App Provider to overal app.js

--- next-backend/pages/_app.js
+++ next-backend/pages/_app.js
@@ -1,15 +1,18 @@
 // pages/_app.js
 import { ProductsProvider } from '../context/ProductsContext.js';
+import { AppProvider } from "../context/context.js";
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ProductsProvider>
-      <Navigation />
-      <Component {...pageProps} />
-      <Footer />
-    </ProductsProvider>
+    <AppProvider>
+      <ProductsProvider>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </ProductsProvider>
+    </AppProvider>
   );
 }
 
